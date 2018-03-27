e16160885c8c5213062bb89310eb90681556a3bb
Elliott Grieco <elliott.grieco@gmail.com>
2018-03-27 17:11:25 -0400

    Adds trustUrl filter test coverage

---
+++ spec/libraryh3lpWidget.filter.spec.js
@@ -0,0 +1,53 @@
+const libraryh3lpWidgetConfig = __fixtures__['libraryh3lpWidgetConfig'];
+
+describe('trustUrl filter', () => {
+
+  let trustUrl, $sce;
+  beforeEach(() => {
+    module('libraryh3lpWidget');
+    inject(function(_trustUrlFilter_, _$sce_) {
+      trustUrl = _trustUrlFilter_;
+      $sce = _$sce_;
+    });
+  });
+
+  const validUrls = [
+    'https://us.libraryh3lp.com/chat/ask@chat.libraryh3lp.com?skin=1',
+    'http://libraryh3lp.com/chat/'
+  ];
+  describe('if the url is valid', () => {
+    it('should call $sce.trustAsResourceUrl with valid URL', () => {
+      spyOn($sce, 'trustAsResourceUrl');
+
+      validUrls.forEach(url => {
+        trustUrl(url);
+        expect($sce.trustAsResourceUrl).toHaveBeenCalledWith(url);
+      });
+    });
+  });
+
+  const invalidUrls = [
+    'http://invalid.url',
+    'http://libraryhelp.com/chat/',
+    'https://libraryh3lp.net/chat/',
+    'ftp://libraryh3lp.com/chat/',
+  ];
+  describe('if the url is invalid', () => {
+    it('should return undefined', () => {
+      invalidUrls.forEach(url => {
+        const filtered = trustUrl(url);
+        expect(filtered).toBe(undefined);
+      });
+
+    });
+
+    it('should not call $sce.trustAsResourceURL', () => {
+      spyOn($sce, 'trustAsResourceUrl');
+
+      invalidUrls.forEach(url => {
+        trustUrl(url);
+        expect($sce.trustAsResourceUrl).not.toHaveBeenCalled();
+      });
+    });
+  });
+});
