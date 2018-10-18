0b6753bcfac0cb1ff8f876234c95a6f141d6aac8
Elliott Grieco <elliott.grieco@nyu.edu>
2018-10-18 17:16:04 -0400

    Fixes chat movement behavior

--- src/js/libraryh3lp-widget.module.js
+++ src/js/libraryh3lp-widget.module.js
@@ -6,33 +6,60 @@ angular
   // Add the libraryh3lp url to trusted url sources
   // so angular doesn't block it from an iframe
   .filter('trustUrl', ['$sce', function ($sce) {
-    return function(url) {
+    return function (url) {
       if (/^http(s)?:\/\/(.+\.)?libraryh3lp\.com.+$/.test(url)) {
         return $sce.trustAsResourceUrl(url);
       }
     };
   }])
-  // Controller for the component below
-  .controller('libraryh3lpWidgetController', ['libraryh3lpWidgetConfig', '$scope', function(libraryh3lpWidgetConfig, $scope) {
-    const ctrl = this;
-    this.$onInit = () => {
+  .factory('libraryh3lpWidgetResultsList', function () {
+    // A factory that is used to get/update state of 'results' across components
+    let length = 0;
+    return {
+      updateLength(newlength) {
+        length = newlength;
+      },
+      getLength: () => length,
+    };
+  })
+  .controller('libraryh3lpWidgetController', ['libraryh3lpWidgetConfig', '$scope', 'libraryh3lpWidgetResultsList', function (libraryh3lpWidgetConfig, $scope, libraryh3lpWidgetResultsList) {
+    // controller for the h3lp widget itself
+    let prevResultsListLength;
+    this.$onInit = function () {
       $scope.config = libraryh3lpWidgetConfig;
-      // Do facets exist?
-      $scope.facetsExist = (() => {
-        try {
-          return (ctrl.parentCtrl.searchService.facetService.results.length > 0);
-        } catch (e) {
-          return false;
-        }
-      })();
-      // Add the bottom padding class if there are facets
-      $scope.bottomPadding = { ["chat-bottom-padding"]: $scope.facetsExist };
+      $scope.klasses = { 'chat-bottom-padding': false };
+      prevResultsListLength = libraryh3lpWidgetResultsList.getLength();
+    };
+
+    this.$doCheck = function () {
+      const newResultsListLength = libraryh3lpWidgetResultsList.getLength();
+      if (!angular.equals(newResultsListLength, prevResultsListLength)) {
+        $scope.klasses['chat-bottom-padding'] = !!newResultsListLength;
+      }
     };
   }])
   .component('prmSilentLoginAfter', {
-    bindings: {
-      parentCtrl: '<'
-    },
     controller: 'libraryh3lpWidgetController',
     template
+  })
+  .controller('libraryh3lpWidgetResultsListController', ['libraryh3lpWidgetResultsList', function (libraryh3lpWidgetResultsList) {
+    // controller whose pure function is to maintain updated results length in the libraryh3lpWidgetResultsList factory
+    const ctrl = this;
+    let prevResultsListLength;
+    this.$onInit = function () {
+      prevResultsListLength = ctrl.parentCtrl.searchService.facetService.results.length;
+      libraryh3lpWidgetResultsList.updateLength(prevResultsListLength);
+    };
+
+    this.$doCheck = function () {
+      if (!angular.equals(prevResultsListLength, ctrl.parentCtrl.searchService.facetService.results.length)) {
+        libraryh3lpWidgetResultsList.updateLength(ctrl.parentCtrl.searchService.facetService.results.length);
+      }
+    };
+  }])
+  .component('prmExploreMainAfter', {
+    bindings: {
+      parentCtrl: '<',
+    },
+    controller: 'libraryh3lpWidgetResultsListController',
   });
