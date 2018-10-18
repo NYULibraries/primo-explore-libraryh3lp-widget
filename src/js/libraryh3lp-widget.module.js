fe5716007ff51bb0f5294d48df4e28ac8c02d16d
Elliott Grieco <elliott.grieco@nyu.edu>
2018-10-18 11:39:02 -0400

    Uses prm-silent-login-after instead for consistency across views

--- src/js/libraryh3lp-widget.module.js
+++ src/js/libraryh3lp-widget.module.js
@@ -29,7 +29,7 @@ angular
       $scope.bottomPadding = { ["chat-bottom-padding"]: $scope.facetsExist };
     };
   }])
-  .component('prmExploreMainAfter', {
+  .component('prmSilentLoginAfter', {
     bindings: {
       parentCtrl: '<'
     },
